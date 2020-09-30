5f2dd931142b1da3ab6e572ffe4ff2f598217f68
Yuta Kawasumi <ytkwsm@gmail.com>
2020-09-30 19:33:09 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
@@ -17,23 +16,5 @@ module.exports = {
         use: 'babel-loader'
       }
     ]
-  },
-  optimization: {
-      minimize: true,
-    minimizer: [
-        new TerserPlugin({
-          extractComments: true,
-          cache: true,
-          parallel: true,
-          sourceMap: true, // Must be set to true if using source-maps in production
-          terserOptions: {
-            // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-             extractComments: 'all',
-             compress: {
-                 drop_console: true,
-             },
-      }
-        }),
-      ]
   }
 }
